93f3e61278bd404602be9ff816e36dea4e186d3d
dude-1977i <janaksmail@gmail.com>
2021-03-08 21:18:40 +0100

    Fix

--- roombook/views.py
+++ roombook/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from roombook.models import Rom
+
 # Create your views here.
 def post_list(request):
-    return render(request, 'roombook/post_list.html', {})
+    return render(request, 'roombook/post_list.html', context={"rooms": Rom.objects.all()})
